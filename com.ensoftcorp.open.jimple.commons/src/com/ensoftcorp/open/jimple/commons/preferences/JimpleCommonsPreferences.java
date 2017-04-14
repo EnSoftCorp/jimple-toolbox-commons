92759f3cdb0c3fbd54120640cfe5f2d98921faa5
Ben Holland <benjholla@gmail.com>
2017-04-14 12:24:42 -0500

    added feature to programmatically set preferences

--- com.ensoftcorp.open.jimple.commons/src/com/ensoftcorp/open/jimple/commons/preferences/JimpleCommonsPreferences.java
+++ com.ensoftcorp.open.jimple.commons/src/com/ensoftcorp/open/jimple/commons/preferences/JimpleCommonsPreferences.java
@@ -17,6 +17,19 @@ private static boolean initialized = false;
 	public static final Boolean RECOVER_DECOMPILED_LOOPS_DEFAULT = false;
 	private static boolean decompiledLoopRecoveryValue = RECOVER_DECOMPILED_LOOPS_DEFAULT;
 	
+	/**
+	 * Configures whether or not to run recovery of decompiled loop analysis
+	 */
+	public static void enableDecompiledLoopRecovery(boolean enabled){
+		IPreferenceStore preferences = Activator.getDefault().getPreferenceStore();
+		preferences.setDefault(RECOVER_DECOMPILED_LOOPS, enabled);
+		loadPreferences();
+	}
+	
+	/**
+	 * Returns true if recovery of decompiled loop analysis is enabled
+	 * @return
+	 */
 	public static boolean isDecompiledLoopRecoveryEnabled(){
 		if(!initialized){
 			loadPreferences();
@@ -29,18 +42,48 @@ private static boolean initialized = false;
 	 */
 	public static final String COMPUTE_LOOP_BOUNDARIES = "COMPUTE_LOOP_BOUNDARIES";
 	public static final Boolean COMPUTE_LOOP_BOUNDARIES_DEFAULT = false;
-	private static boolean computeLoopBoundariesDefault = COMPUTE_LOOP_BOUNDARIES_DEFAULT;
+	private static boolean computeLoopBoundariesValue = COMPUTE_LOOP_BOUNDARIES_DEFAULT;
 	
+	/**
+	 * Configures whether or not to run loop boundary analysis
+	 */
+	public static void enableLoopBoundaryAnalysis(boolean enabled){
+		IPreferenceStore preferences = Activator.getDefault().getPreferenceStore();
+		preferences.setDefault(COMPUTE_LOOP_BOUNDARIES, enabled);
+		loadPreferences();
+	}
+	
+	/**
+	 * Returns true if loop boundary analysis is enabled
+	 * @return
+	 */
 	public static boolean isComputeLoopBoundariesEnabled(){
 		if(!initialized){
 			loadPreferences();
 		}
-		return computeLoopBoundariesDefault;
+		return computeLoopBoundariesValue;
 	}
 	
 	@Override
 	public void initializeDefaultPreferences() {
 		IPreferenceStore preferences = Activator.getDefault().getPreferenceStore();
+		setDefaults(preferences);
+	}
+	
+	/**
+	 * Restores the default preferences
+	 */
+	public static void restoreDefaults(){
+		IPreferenceStore preferences = Activator.getDefault().getPreferenceStore();
+		setDefaults(preferences);
+		loadPreferences();
+	}
+	
+	/**
+	 * Helper method to set default preferences
+	 * @param preferences
+	 */
+	private static void setDefaults(IPreferenceStore preferences) {
 		preferences.setDefault(RECOVER_DECOMPILED_LOOPS, RECOVER_DECOMPILED_LOOPS_DEFAULT);
 		preferences.setDefault(COMPUTE_LOOP_BOUNDARIES, COMPUTE_LOOP_BOUNDARIES_DEFAULT);
 	}
@@ -52,7 +95,7 @@ private static boolean initialized = false;
 		try {
 			IPreferenceStore preferences = Activator.getDefault().getPreferenceStore();
 			decompiledLoopRecoveryValue = preferences.getBoolean(RECOVER_DECOMPILED_LOOPS);
-			computeLoopBoundariesDefault = preferences.getBoolean(COMPUTE_LOOP_BOUNDARIES);
+			computeLoopBoundariesValue = preferences.getBoolean(COMPUTE_LOOP_BOUNDARIES);
 		} catch (Exception e){
 			Log.warning("Error accessing jimple commons analysis preferences, using defaults...", e);
 		}
